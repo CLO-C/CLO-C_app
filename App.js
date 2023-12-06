056fa3e129d0f692ce74e5bce60855bad537563a
9-coding <9.coding.lab@gmail.com>
2023-12-06 16:40:08 +0900

    FEAT: get real-time weather information

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default function App() {
                         headerShown: false,
                     }}
                 >
-                    <Stack.Screen name="StartScreen" component={ImageUploadScreen} />
+                    <Stack.Screen name="StartScreen" component={StartScreen} />
                     <Stack.Screen name="LoginScreen" component={LoginScreen} />
                     <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
                     {/* Integrate BottomTabNavigator as a screen */}
