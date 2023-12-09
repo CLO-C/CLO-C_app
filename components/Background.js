41816e0ad4d6b9297fa2d39a63d87bd739debd5d
SeohyunSim <angelsh0805@gachon.ac.kr>
2023-12-09 18:43:37 +0900

    캘린더 해당 일자 데이터 가져오기 (UI 미적용)

--- components/Background.js
+++ components/Background.js
@@ -5,6 +5,8 @@ import { theme } from '../core/theme'
 export default function Background({ children }) {
     return (
         <ImageBackground
+            source={require('../assets/background_dot.png')}
+            resizeMode="repeat"
             style={styles.background}
         >
             <KeyboardAvoidingView style={styles.container} behavior="padding">
